a415c1dda3f163305e3d07a7af8f6acec3cb5136
Rayarasp <rayaraps93@gmail.com>
2025-05-06 11:53:10 -0300

    Pagcad

--- src/pages/cadastrar/index.jsx
+++ src/pages/cadastrar/index.jsx
@@ -1,17 +1,72 @@
+import CustomInput from "@/components/CustomInput";
+import CustomSelect from "@/components/CustomSelect";
 import PageWrapper from "@/components/PageWrapper";
 
-export default function Cadastrar() {
+export default function Cadastrar(){
     return (
-    
-        <PageWrapper showButton={false}>
+        <PageWrapper  showButton={false}>
             <div className="w-full h-full py-[40px] px-[70px] flex flex-col">
-                <h1 className="text-[40px] font-bold text-[#9A86f4]">Adicione um novo Filme</h1>
-                <p className="text-[20px] text-gray-500">Preencha o formulário abaixo para adicionar filmes à sua coleção</p>
+                <h1 className="text-[40px] font-bold text-[#9A86F4]">Adicione um novo filme</h1>
+                <p className="text-[20px] text-gray-500">Preencha o formulário abaixo para adicionar um novo filme à sua coleção.</p>
                 <div className="w-full flex justify-center pt-[40px]">
-                    <form className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-3"></form>
+                    <form className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-6 gap-4">
+                        <CustomInput 
+                            label="Título"
+                            placeholder="Digite o título do filme"
+                            type="text"
+                        />
+                        <CustomInput 
+                            label="Diretor"
+                            placeholder="Digite o nome do diretor"
+                            type="text"
+                        />
+                        <div className="w-full flex gap-4">
+                            <div className="w-[50%]">
+                                <CustomInput
+                                    label="Ano"
+                                    placeholder="Digite o ano de lançamento"
+                                    type="number"
+                                />
+                            </div>
+                            <div className="w-[50%]">
+                                <CustomSelect
+                                    label="Gênero"
+                                    options={[
+                                        "Ação", 
+                                        "Comédia", 
+                                        "Drama", 
+                                        "Terror", 
+                                        "Ficção Científica", 
+                                        "Romance"
+                                    ]}
+                                />
+                            </div>
+                        </div>
+                        <div className="w-[30%] flex gap-2">
+                            <div className="w-[70%]">
+                                <CustomInput
+                                    label="Nota"
+                                    placeholder="0"
+                                    type="number"
+                                />
+                            </div>
+                            <p className="flex mt-10">10 / 10</p>
+                        </div>
+                        <div className="w-full flex flex-col gap-2">
+                            <label className="text-[17px] font-bold">Sinopse</label>
+                            <textarea 
+                                className="w-full h-[100px] min-h-[100px] max-h[150px] bg-[#141414] border border-[#ffffff1a] rounded-lg p-2 outline-none focus:border-purple-400"
+                            ></textarea>
+                        </div>
+                        <CustomInput
+                            label="Banner"
+                            placeholder="URL da imagem"
+                            type="text" 
+                        />
+                        <div className="w-full flex justify-end gap-4"></div>
+                    </form>
                 </div>
             </div>
-    
         </PageWrapper>
     )
 }
